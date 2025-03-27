d5dfdda67cc2e59a97e86a798f1cb62c3ac9c949
fburman666 <fburman@gmail.com>
2025-03-27 09:38:59 +0100

    hej

---
+++ Userstories.py
@@ -0,0 +1,46 @@
+"""[U1] Som en användare vill jag kunna skapa och ta bort widgets för timer och anteckning
+så att jag kan använda funktionerna för tid och text
+
+[U2] Som en användare vill jag kunna byta plats på 2 widgets
+så att de placeras precis som jag vill ha det
+
+[U3] Som en användare vill jag kunna ändra tidsinställning på timern
+så att önskvärd tid kan användas
+
+[U4] Som en användare vill jag kunna starta, pausa och återställa timern
+så att jag kan använda timern för nedräkning
+
+[U5] Som en användare vill jag kunna lägga till test
+så att inte glömmer viktiga punkter
+
+[U6] Som en användare vill jag kunna ändra temafärg
+så att utseendet blir som jag vill ha det
+
+
+[A1.1] Det går att skapa widget för timer
+[A1.2] Det går att skapa widget för anteckningar
+[A2.1] Det går att byta plats på 2 widgets
+[A3.1] Det går att ändra tid i timer widget
+[A4.1] Det går att starta timern
+[A4.2] Det går att pausa timern
+[A4.3] Det går att återställa timern
+[A5.1] Det går att lägga till text i text widget
+[A6.1] Det går att ändra temafärg
+
+[T1.1] Klicka på 'Add timer'
+       Kontrollera att knappen 'Start' är synlig
+[T1.2] Klicka på 'Add note'
+       Kontrollera att textfältet är synligt
+[T2.1] Klicka på 'Add timer'.
+       Klicka på 'Add note'
+       Klicka på 'icon up' i text widget
+       Kontrollera att text widget hamnar över time widget
+[T3.1] Klicka på 'Add timer'
+       Sätt 'row time' till 10
+       Kontrollera att 'row time' blir 10
+
+
+
+
+
+"""
